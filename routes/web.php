a0fede6ac7cd265798caf9b68328bc3d704a2a7d
mustak <mustakhridoy@gmail.com>
2019-07-21 03:43:41 +0600

    Admin Controller added

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,32 @@ Route::get('post', function(){
 })->name('post');
 
 
-Route::get('admin/home', function() {
-    return view('admin.home');
-})->name('admin');
 
-Route::get('admin/post', function() {
-    return view('admin.post.post');
-});
+Route::resource('admin/post', 'Admin\PostController');
+Route::resource('admin/tag', 'Admin\TagController');
+Route::resource('admin/category', 'Admin\CategoryController');
 
 
-Route::get('admin/tag', function() {
-    return view('admin.tag.tag');
-});
 
 
-Route::get('admin/category', function() {
-    return view('admin.category.category');
-});
+
+
+// Route::get('admin/home', function() {
+//     return view('admin.home');
+// })->name('admin');
+
+// Route::get('admin/post', function() {
+//     return view('admin.post.post');
+// });
+
+
+// Route::get('admin/tag', function() {
+//     return view('admin.tag.tag');
+// });
+
+
+// Route::get('admin/category', function() {
+//     return view('admin.category.category');
+// });
 
 
