7adeb171ccc308cf7c9ccbf41ed27ee0e9b1b30e
mustak <mustakhridoy@gmail.com>
2019-07-25 02:35:16 +0600

    show post on frontend

--- app/Http/Controllers/User/PostController.php
+++ app/Http/Controllers/User/PostController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Model\user\post;
+
 class PostController extends Controller
 {
-    public function index()
+    public function post(post $post)
     {
-        return view('user.post');
+        return view('user.post', compact('post'));
     }
 }
