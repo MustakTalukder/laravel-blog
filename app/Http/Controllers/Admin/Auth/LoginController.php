dc1fe009e1ae45bd6cd513b4787ef3c55e028889
mustak <mustakhridoy@gmail.com>
2019-07-30 17:43:48 +0600

    admin login auth

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'admin/home';
 
     /**
      * Create a new controller instance.
@@ -59,5 +59,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    protected function guard()
+    {
+        return Auth::guard('admin');
     }
 }
--- app/Model/admin/admin.php
+++ app/Model/admin/admin.php
@@ -2,9 +2,12 @@
 
 namespace App\Model\admin;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
-class admin extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class admin extends Authenticatable
 {
     //
+    use Notifiable;
 }
